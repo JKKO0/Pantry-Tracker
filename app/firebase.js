1303bbf97423207d81ce90feb1fb9ee8dea93a92
JKKO0 <jasur.halilov123@gmail.com>
2024-08-05 01:53:23 -0400

    2nd commit

--- app/firebase.js
+++ app/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: YOUR_API_KEY,
-    authDomain: YOUR_AUTH_DOMAIN,
-    projectId: YOUR_PROJECT_ID,
-    storageBucket: YOUR_STORAGE_BUCKET,
-    messagingSenderId: YOU_MESSAGING_SENDER_ID,
-    appId: APP_ID,
+    // apiKey: YOUR_API_KEY,
+    // authDomain: YOUR_AUTH_DOMAIN,
+    // projectId: YOUR_PROJECT_ID,
+    // storageBucket: YOUR_STORAGE_BUCKET,
+    // messagingSenderId: YOU_MESSAGING_SENDER_ID,
+    // appId: APP_ID,
 };
 
 // Initialize Firebase
